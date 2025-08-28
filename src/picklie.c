80d693aba249c08e5a128ff9f13ebe5928462680
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-08-28 19:01:20 +0300

    picklie.c: get mouse cursor position or exit program on key press
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- src/picklie.c
+++ src/picklie.c
@@ -5,17 +5,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <conio.h>
 #include <windows.h>
+
+#define KEY_SPACE 0x20
+#define KEY_ESCAPE 0x1B
 
 int main(int argc, char** argv)
 {
+    POINT CursorPoint;
+    char Command = '\0';
+    
     for (int i = 0; i < argc; i++)
     {
         if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version"))
         {
             printf("Picklie v%s\nCopyright (c) 2025 Ivan Movchan\nhttps://github.com/ivan-movchan/picklie\n", VERSION);
             return 0;
+        };
+    };
+    
+    while (Command != KEY_ESCAPE)
+    {
+        Command = getch();
+        
+        if (Command == KEY_SPACE)
+        {
+            if (GetCursorPos(&CursorPoint) != 0)
+            {
+                printf("%dx, %dy\n", CursorPoint.x, CursorPoint.y);
+            }
+            else
+            {
+                printf("Failed to get cursor position (0x%08X).\n", GetLastError());
+            };
         };
     };
     
